b19c78a7dc24089e3417bc5000d703545cd3e6b0
paruyrmovsesyan <paruyrmovsesyan@users.noreply.github.com>
2026-06-12 00:54:20 +0400

    Chore: bump service worker cache to v19 to flush index.html loader update

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = "worship-v18";
+const CACHE_VERSION = "worship-v19";
 const STATIC_CACHE = `${CACHE_VERSION}-static`;
 const RUNTIME_CACHE = `${CACHE_VERSION}-runtime`;
 const DATA_CACHE = `${CACHE_VERSION}-data`;
