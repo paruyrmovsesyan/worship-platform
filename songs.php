fd933ea325eb437dc7841494c2e0706a80c6b59b
paruyrmovsesyan <paruyrmovsesyan@users.noreply.github.com>
2026-06-12 00:33:36 +0400

    Fix: remove stray closing brace in songs.php leftover from dictionary migration

--- songs.php
+++ songs.php
@@ -10,7 +10,6 @@ if (isset($_GET['lang']) && in_array($_GET['lang'], ['hy', 'ru', 'en'])) {
     exit;
 }
 $adminLang = $_COOKIE['admin_lang'] ?? 'hy';
-}
 
 // Generate JSON for JS translations
 $js_i18n_keys = [
